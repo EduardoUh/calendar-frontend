5b2ac3319986ce12e202009486d9e30bab3e3d83
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-02-24 14:45:48 -0600

    fix(api): change the env variable name for the correct one

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { config } from "dotenv";
 
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_LOCAL_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 // interceptors allow us to edit the headers of the query, it can be in the request headers or in the response headers
